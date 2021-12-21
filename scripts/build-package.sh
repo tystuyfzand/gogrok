58337d8976f40f1c8811a88c99abfaec7cab9297
Tyler <tystuyfzand@gmail.com>
2021-12-20 20:17:01 -0500

    Update packaging script/architectures

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,13 +1,21 @@
+PACKAGE_ARCH="$ARCH"
+
+if [ "$PACKAGE_ARCH" = "arm" ]; then
+  PACKAGE_ARCH="armhf"
+elif [ "$PACKAGE_ARCH" = "386" ]; then
+  PACKAGE_ARCH="i386"
+fi
+
 fpm -s dir -t deb -p /build/gogrok_${DRONE_TAG}_${ARCH}.deb \
-    -n gogrok -v $DRONE_SEMVER -a $ARCH \
+    -n gogrok -v "$DRONE_SEMVER" -a "$PACKAGE_ARCH" \
     --deb-priority optional --force \
     --deb-compression gz --verbose \
     --description "Gogrok: Open-source ngrok alternative" \
-    -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Paste.ee" \
-    -a $ARCH /build/gogrok_linux_${ARCH}=/usr/bin/gogrok
+    -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Meow.tf" \
+    /build/gogrok_linux_${ARCH}=/usr/bin/gogrok
 
 fpm -s dir -t rpm -p /build/gogrok_${DRONE_TAG}_${ARCH}.rpm \
-    -n gogrok -v $DRONE_SEMVER -a $ARCH \
+    -n gogrok -v $DRONE_SEMVER -a "$PACKAGE_ARCH" \
     --description "Gogrok: Open-source ngrok alternative" \
-    -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Paste.ee" \
-    -a $ARCH /build/gogrok_linux_${ARCH}=/usr/bin/gogrok
+    -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Meow.tf" \
+    /build/gogrok_linux_${ARCH}=/usr/bin/gogrok
